21f0056911273517101ed4c6fedeebccaf03ba06
Ahmedelabasyy <ahmedelabasy662@gmail.com>
2023-10-12 10:39:08 +0300

    design enhancment

--- src/page/User.jsx
+++ src/page/User.jsx
@@ -31,7 +31,7 @@ export default function User() {
       className="w-full h-screen flex justify-center items-center"
       style={{ backgroundImage: "url('/bg.svg')" }}
     >
-      <div className=" w-[50%] rounded-3xl py-[8rem] bg-[#185860] flex justify-center items-center ">
+      <div className=" w-[50%] rounded-3xl py-[7rem] bg-[#185860] flex justify-center items-center ">
         <div className="w-[411px] flex flex-col items-center gap-10">
           <div className="text-center ali">
             <svg
